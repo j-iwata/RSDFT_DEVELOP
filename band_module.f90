1913d41eec7966a3c3100ee88acacad32a4ad78f
iwata <iwata@d3f10982-2151-483e-8c64-340d25a337fb>
2013-09-25 08:45:36 +0000

    - k-parallel M    band_module.f90
    git-svn-id: file:///Users/svn/RSDFT/branches/ss_and_cpmd_kw@293 d3f10982-2151-483e-8c64-340d25a337fb

--- band_module.f90
+++ band_module.f90
@@ -1,5 +1,7 @@
 MODULE band_module
 
+  use watch_module
+
   implicit none
 
   PRIVATE
@@ -172,18 +174,22 @@ CONTAINS
     integer,intent(IN) :: MBV_in
     logical,intent(IN) :: disp_switch
     integer :: MBV,nktrj,i,j,k,s,n,ibz,ierr,iktrj,iter,Diter_band
-    integer :: iktrj_0,iktrj_1,iktrj_2,iktrj_00,iktrj_tmp
+    integer :: iktrj_0,iktrj_1,iktrj_2,iktrj_00,iktrj_tmp,ireq
     integer,allocatable :: ir_k(:),id_k(:)
-    real(8) :: dak(3),sum0,sum1,max_err
+    real(8) :: dak(3),sum0,sum1,max_err,max_err0
     real(8),allocatable :: ktrj(:,:),esp0(:,:,:),pxyz(:,:,:,:)
     real(8),allocatable :: kbb_tmp(:,:),esp_tmp(:,:,:),esp0_tmp(:,:,:)
-    complex(8),allocatable :: unk0(:,:,:)
-    logical :: disp_switch_parallel_bak
+    complex(8),allocatable :: unk0(:,:,:),unk00(:,:,:)
+    logical :: disp_switch_parallel_bak,flag_end
+    character(32) :: file_ovlp
+    character(5) :: cc
 
     disp_switch_parallel_bak = disp_switch_parallel
 !    disp_switch_parallel = .false.
 
     Diter_band = maxiter_band
+
+    flag_end = .false.
 
     MBV=MBV_in
     if ( MBV < 1 .or. mb_band < MBV ) MBV=1
@@ -235,7 +241,7 @@ CONTAINS
        write(*,*) "sum(ir_k),nktrj=",sum(ir_k),nktrj
     end if
 
-    if ( mb_band <= MB ) then
+    if ( MB < mb_band ) then
        call modify_mb
        call modify_arraysize
     end if
@@ -247,22 +253,33 @@ CONTAINS
     allocate( esp0(MB,MBZ,MSP)               ) ; esp0=0.d0
     allocate( unk0(ML_0:ML_1,MB,MSP_0:MSP_1) ) ; unk0=0.d0
 
-    k=MBZ_0
+!    k=MBZ_0
 
     if ( myrank == 0 ) then
        open(unit_band_eigv,file="band_eigv")
-       open(unit_band_ovlp,file="band_ovlp")
        open(unit_band_dedk,file="band_dedk")
        write(unit_band_eigv,'(1x,3f20.15)') bb(1:3,1)
        write(unit_band_eigv,'(1x,3f20.15)') bb(1:3,2)
        write(unit_band_eigv,'(1x,3f20.15)') bb(1:3,3)
     end if
 
+    if ( np_bzsm == 1 ) then
+       if ( myrank == 0 ) then
+          open(unit_band_ovlp,file="band_ovlp")
+       end if
+    else
+       write(cc,'(i5.5)') myrank
+       file_ovlp = "band_ovlp_"//trim(adjustl(cc))
+       if ( myrank_g == 0 .and. myrank_b == 0 ) then
+          open(unit_band_ovlp,file=file_ovlp)
+       end if
+    end if
+
     iktrj_0 = id_k(myrank_k)+1
     iktrj_1 = id_k(myrank_k)+ir_k(myrank_k)
     iktrj_2 = id_k(myrank_k)+maxval(ir_k)
 
-    do iktrj=iktrj_0,iktrj_2
+    loop_iktrj : do iktrj = iktrj_0, iktrj_2
 
        iktrj_00 = id_k(0) + iktrj - iktrj_0 + 1
 
@@ -283,7 +300,7 @@ CONTAINS
        do iter=1,Diter_band
           if ( disp_switch ) write(*,'(a60,"band_iter=",i3)') repeat("-",60),iter
           do s=MSP_0,MSP_1
-             call conjugate_gradient(ML_0,ML_1,MB,k,s,Ncg,iswitch_gs &
+             call conjugate_gradient(ML_0,ML_1,MB,MBZ_0,s,Ncg,iswitch_gs &
                   ,unk(ML_0,1,MBZ_0,s),esp(1,MBZ_0,s),res(1,MBZ_0,s))
              call gram_schmidt_t(1,MB,MBZ_0,s)
 !            call subspace_diag_la(MBZ_0,s)
@@ -291,6 +308,9 @@ CONTAINS
           end do
           max_err = maxval( abs(  esp(1:mb2_band,MBZ_0,MSP_0:MSP_1) &
                                 -esp0(1:mb2_band,MBZ_0,MSP_0:MSP_1) ) )
+          if ( iktrj_1 < iktrj ) max_err=0.d0
+          call mpi_allreduce(max_err,max_err0,1,MPI_REAL8,MPI_MAX,comm_spin,ierr)
+          call mpi_allreduce(max_err0,max_err,1,MPI_REAL8,MPI_MAX,comm_bzsm,ierr)
           if ( max_err < esp_conv_tol ) then
              if ( disp_switch ) write(*,*) "iktrj,max_err,iter=",iktrj,max_err,iter
              exit
@@ -298,20 +318,61 @@ CONTAINS
              stop "band is not converged"
           end if
           esp0(:,:,:)=esp(:,:,:)
+          call global_watch(flag_end)
+          if ( flag_end ) then
+             if ( myrank == 0 ) write(*,*) "etime limit !!!"
+             exit loop_iktrj
+          end if
        end do ! iter
 
        call esp_gather(MB,Nbzsm,MSP,esp)
 
        call gather_wf
 
-       do s=MSP_0,MSP_1
-          if ( iktrj > 1 .and. np_bzsm == 1 ) then
 #ifndef _DRSDFT_
-             call calc_overlap(ML_0,ML_1,MB_0,MB_1,MB,unk(:,1,MBZ_0,s),unk0(:,1,s))
-#endif
+       if ( iktrj_0 < iktrj ) then
+          if ( np_bzsm == 1 ) then
+             if ( myrank == 0 ) then
+                write(unit_band_ovlp,'(1x,2i6,3f20.12)') iktrj,MB,kbb(1:3,MBZ_0)
+             end if
+          else
+             if ( myrank_g == 0 .and. myrank_b == 0 ) then
+                write(unit_band_ovlp,'(1x,2i6,3f20.12)') iktrj,MB,kbb(1:3,MBZ_0)
+             end if
+          end if
+       end if
+       do s=MSP_0,MSP_1
+          if ( np_bzsm == 1 ) then
+             if ( 1 < iktrj .and. iktrj <= iktrj_1 ) then
+                call calc_overlap(ML_0,ML_1,MB_0,MB_1,MB,unk(:,1,MBZ_0,s),unk0(:,1,s))
+             end if
+          else
+             if ( iktrj_0 < iktrj .and. iktrj <= iktrj_1 ) then
+                call calc_overlap_kpara(ML_0,ML_1,MB_0,MB_1,MB,unk(:,1,MBZ_0,s),unk0(:,1,s))
+             end if
+             if ( iktrj == iktrj_0 ) then
+                if ( .not.allocated(unk00) ) then
+                   allocate( unk00(ML_0:ML_1,MB,MSP_0:MSP_1) )
+                   unk00=0.d0
+                end if
+                unk00(:,:,s)=unk(:,:,MBZ_0,s)
+             end if
+             if ( iktrj == iktrj_1 ) then
+                call send_wf_band(ML_1-ML_0+1,MB,unk00(ML_0,1,s))
+                unk0(:,:,s) = unk(:,:,MBZ_0,s)
+                call recv_wf_band(ML_1-ML_0+1,MB,unk(ML_0,1,MBZ_0,s))
+                if ( myrank_k < np_bzsm-1 ) then
+                   if ( myrank_g == 0 .and. myrank_b == 0 ) then
+                      write(unit_band_ovlp,'(1x,2i6,3f20.12)') iktrj+1,MB,ktrj(1:3,iktrj+1)
+                   end if
+                   call calc_overlap_kpara(ML_0,ML_1,MB_0,MB_1,MB,unk(:,1,MBZ_0,s),unk0(:,1,s))
+                end if
+                if ( allocated(unk00) ) deallocate(unk00)
+             end if
           end if
           unk0(:,:,s) = unk(:,:,MBZ_0,s)
        end do
+#endif
 
        call prep_rvk_ps_nloc2(MBZ_0,MBZ_0,kbb(1,MBZ_0))
 
@@ -344,7 +405,6 @@ CONTAINS
           if ( iktrj_tmp > nktrj ) exit
           if ( myrank == 0 ) then
              write(unit_band_eigv,'(1x,2i6,3f20.12,i8)') iktrj_tmp,mb2_band,kbb_tmp(1:3,ibz),MBV
-             write(unit_band_ovlp,'(1x,2i6,3f20.12)') iktrj_tmp,MB,kbb_tmp(1:3,ibz)
              write(unit_band_dedk,'(1x,2i6,3f20.12)') iktrj_tmp,mb2_band,kbb_tmp(1:3,ibz)
           end if
           do n=1,mb2_band
@@ -361,12 +421,20 @@ CONTAINS
           end do ! n
        end do ! ibz
 
-    end do ! iktrj
+    end do loop_iktrj
 
     if ( myrank == 0 ) then
        close(unit_band_dedk)
-       close(unit_band_ovlp)
        close(unit_band_eigv)
+    end if
+    if ( np_bzsm == 1 ) then
+       if ( myrank == 0 ) then
+          close(unit_band_ovlp)
+       end if
+    else
+       if ( myrank_g == 0 .and. myrank_b == 0 ) then
+          close(unit_band_ovlp)
+       end if
     end if
 
     deallocate( unk0 )
@@ -383,6 +451,57 @@ CONTAINS
 
   END SUBROUTINE band
 
+  SUBROUTINE send_wf_band(mm,nn,f)
+    use parallel_module
+    implicit none
+    integer,intent(IN) :: mm,nn
+    complex(8),intent(IN) :: f(mm,nn)
+    integer :: ierr,ireq,istatus(MPI_STATUS_SIZE)
+    if ( myrank_k > 0 ) then
+       call MPI_ISEND(f,mm*nn,MPI_COMPLEX16,myrank_k-1,1,comm_bzsm,ireq,ierr)
+       call MPI_WAIT(ireq,istatus,ierr)
+    end if
+  END SUBROUTINE send_wf_band
+  SUBROUTINE recv_wf_band(mm,nn,f)
+    use parallel_module
+    implicit none
+    integer,intent(IN) :: mm,nn
+    complex(8),intent(OUT) :: f(mm,nn)
+    integer :: ierr,ireq,istatus(MPI_STATUS_SIZE)
+    if ( myrank_k < np_bzsm-1 ) then
+       call MPI_IRECV(f,mm*nn,MPI_COMPLEX16,myrank_k+1,1,comm_bzsm,ireq,ierr)
+       call MPI_WAIT(ireq,istatus,ierr)
+    end if
+  END SUBROUTINE recv_wf_band
+
+  SUBROUTINE write_data_band(iktrj,rank,mm,nn,unk)
+    implicit none
+    integer,intent(IN) :: iktrj,rank,mm,nn
+    complex(8),intent(IN) :: unk(mm,nn)
+    character(5) :: c1,c2
+    character(32) :: filename
+    write(c1,'(i5.5)') iktrj
+    write(c2,'(i5.5)') rank
+    filename = "wf_band."//trim(adjustl(c1))//trim(adjustl(c2))
+    open(10+rank,file=filename,form='unformatted')
+    write(10+rank) unk
+    call flush(10+rank)
+    close(10+rank)
+  END SUBROUTINE write_data_band
+
+  SUBROUTINE read_data_band(iktrj,rank,mm,nn,unk)
+    implicit none
+    integer,intent(IN) :: iktrj,rank,mm,nn
+    complex(8),intent(OUT) :: unk(mm,nn)
+    character(5) :: c1,c2
+    character(32) :: filename
+    write(c1,'(i5.5)') iktrj-1
+    write(c2,'(i5.5)') rank
+    filename = "wf_band."//trim(adjustl(c1))//trim(adjustl(c2))
+    open(10+rank,file=filename,form='unformatted')
+    read(10+rank) unk
+    close(10+rank)
+  END SUBROUTINE read_data_band
 
   SUBROUTINE calc_overlap(n1,n2,m1,m2,mm,f1,f0)
     use parallel_module
@@ -446,6 +565,69 @@ CONTAINS
     deallocate( maxloc10 )
 
   END SUBROUTINE calc_overlap
+
+  SUBROUTINE calc_overlap_kpara(n1,n2,m1,m2,mm,f1,f0)
+    use parallel_module
+    implicit none
+    integer,intent(IN) :: n1,n2,m1,m2,mm
+    complex(8),intent(IN) :: f1(n1:n2,mm),f0(n1:n2,mm)
+    real(8),allocatable :: sqovlp01(:,:),sqovlp10(:,:),sqovlp10_tmp(:,:)
+    complex(8),allocatable :: zwork(:,:),ovlp01(:,:)
+    complex(8),parameter :: zero=(0.d0,0.d0),one=(1.d0,0.d0)
+    integer,parameter :: hyaku=100, jyuu=10
+    integer :: nn,ierr,ib1,ib2,nib,i,j
+    integer,allocatable :: indx(:),maxloc10(:,:),maxloc10_tmp(:,:)
+
+    nn = n2 - n1 + 1
+
+    allocate( maxloc10(mm,jyuu) ) ; maxloc10=0
+    allocate( sqovlp10(mm,jyuu) ) ; sqovlp10=0.d0
+
+    allocate( zwork(mm,hyaku) )
+    allocate( ovlp01(mm,hyaku) )
+    allocate( sqovlp01(mm,hyaku) )
+    allocate( indx(mm) )
+
+    do ib1=m1,m2,hyaku
+       ib2=min(ib1+hyaku-1,m2,mm)
+       nib=ib2-ib1+1
+       if ( nib < 0 ) cycle
+       call zgemm('C','N',mm,nib,nn,one,f0(n1,1),nn,f1(n1,ib1),nn,zero,zwork(1,1),mm)
+       call mpi_allreduce(zwork,ovlp01,hyaku*mm,mpi_complex16,mpi_sum,comm_grid,ierr)
+       sqovlp01(1:mm,1:nib) = abs( ovlp01(1:mm,1:nib) )**2
+       do j=1,nib
+          call indexx(mm,sqovlp01(1,j),indx)
+          do i=mm,max(mm-jyuu+1,1),-1
+             maxloc10(j-1+ib1,mm-i+1) = indx(i)
+             sqovlp10(j-1+ib1,mm-i+1) = sqovlp01( indx(i),j )
+          end do
+       end do
+    end do ! ib1
+
+    deallocate( indx )
+    deallocate( sqovlp01 )
+    deallocate( ovlp01 )
+    deallocate( zwork )
+
+    allocate( maxloc10_tmp(mm,jyuu) ) ; maxloc10_tmp(:,:)=maxloc10(:,:)
+    allocate( sqovlp10_tmp(mm,jyuu) ) ; sqovlp10_tmp(:,:)=sqovlp10(:,:)
+    call mpi_reduce(maxloc10_tmp,maxloc10,mm*jyuu,mpi_integer,mpi_sum,0,comm_band,ierr)
+    call mpi_reduce(sqovlp10_tmp,sqovlp10,mm*jyuu,mpi_real8,mpi_sum,0,comm_band,ierr)
+    deallocate( sqovlp10_tmp,maxloc10_tmp )
+
+    if ( myrank_g == 0 .and. myrank_b == 0 ) then
+       do i=1,mm
+          write(unit_band_ovlp,'(1x,i6,2x,10i6)') i,( maxloc10(i,j),j=1,jyuu )
+       end do
+       do i=1,mm
+          write(unit_band_ovlp,'(1x,i6,2x,10g15.6)') i,( sqovlp10(i,j),j=1,jyuu )
+       end do
+    end if
+
+    deallocate( sqovlp10 )
+    deallocate( maxloc10 )
+
+  END SUBROUTINE calc_overlap_kpara
 
 #ifdef _OLD_VERSION_TEST_
   SUBROUTINE calc_overlap(n1,n2,m1,m2,mm,f1,f0,maxloc_ovlp)
