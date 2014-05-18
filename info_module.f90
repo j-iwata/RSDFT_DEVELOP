a41a13bfe8dd9f7f1430e653e3f7430610414d7b
iwata <iwata@d3f10982-2151-483e-8c64-340d25a337fb>
2014-05-18 00:11:20 +0000

    - svn revision information MM   info_module.f90
    git-svn-id: file:///Users/svn/RSDFT/branches/ss_and_cpmd_kw@432 d3f10982-2151-483e-8c64-340d25a337fb

--- info_module.f90
+++ info_module.f90
@@ -18,6 +18,7 @@ CONTAINS
     if ( rank == 0 ) then
        open(unit_info,file=file_name)
     end if
+    call header_info
   END SUBROUTINE open_info
 
   SUBROUTINE write_info(info)
@@ -33,5 +34,41 @@ CONTAINS
   SUBROUTINE close_info
     if ( myrank == 0 ) close(unit_info)
   END SUBROUTINE close_info
+
+  SUBROUTINE header_info
+    implicit none
+    character(8)  :: date
+    character(10) :: time
+    date=""
+    time=""
+    call date_and_time(DATE=date,TIME=time)
+    if ( myrank == 0 ) then
+       write(*,*) "(Subversion Revision) $Rev$"
+       write(*,*) "(Subversion Date    ) $Date$"
+       write(*,*) "(Compile date/time) ",date,"/",time
+       write(unit_info,*) "(Subversion Revision) $Rev$"
+       write(unit_info,*) "(Subversion Date    ) $Date$"
+       write(unit_info,*) "(Compile date/time) ",date,"/",time
+#ifdef _DRSDFT_
+       write(*,*) "DRSDFT(REAL8)"
+       write(unit_info,*) "DRSDFT(REAL8)"
+#else
+       write(*,*) "ZRSDFT(COMPLEX16)"
+       write(unit_info,*) "ZRSDFT(COMPLEX16)"
+#endif
+#ifdef _SPLINE_
+       write(*,*) "SPLINE(ps_nloc2_module)"
+       write(unit_info,*) "SPLINE(ps_nloc2_module)"
+#endif
+#ifdef _FFTE_
+       write(*,*) "FFTE(ps_local,hartree)"
+       write(unit_info,*) "FFTE(ps_local,hartree)"
+#endif
+#ifdef _LAPACK_
+       write(*,*) "LAPACK"
+       write(unit_info,*) "LAPACK"
+#endif
+    end if
+  END SUBROUTINE header_info
 
 END MODULE info_module
