fb32a23cef9062a171e6ecf723cf50540e30c486
J. Iwata <iwata@kaki.localhost>
2014-07-09 11:49:51 +0900

    bug ( calc_ggapbe96_c2 ) 	modified:   xc_ggapbe96_module.f90

--- xc_ggapbe96_module.f90
+++ xc_ggapbe96_module.f90
@@ -143,7 +143,8 @@ CONTAINS
 
 ! -- Correlation --
 
-    call calc_GGAPBE96_c( rho, vxc_tmp, Ec_part )
+!    call calc_GGAPBE96_c( rho, vxc_tmp, Ec_part )
+    call calc_GGAPBE96_c2( rho, vxc_tmp, Ec_part )
 
     if ( present(Vc_out) ) then
        Vc_out(ML_0:ML_1,MSP_0:MSP_1) = vxc_tmp(ML_0:ML_1,MSP_0:MSP_1)
@@ -252,7 +253,7 @@ CONTAINS
 
           if ( trho <= zero_density ) cycle
 
-          kf = (3.d0*Pi*Pi*trho)**(1.d0/3.d0)
+          kf = (3.0d0*Pi*Pi*trho)**(1.0d0/3.0d0)
 
           ex_lda = -3.0d0/(4.0d0*Pi)*kf
           vx_lda = -1.0d0/Pi*kf
@@ -548,6 +549,224 @@ CONTAINS
 
     return
   END SUBROUTINE calc_GGAPBE96_c
+
+
+  SUBROUTINE calc_GGAPBE96_c2( rho, vco, Ec )
+    implicit none
+    real(8),intent(IN)  :: rho(ML_0:,:)
+    real(8),intent(OUT) :: vco(ML_0:,:), Ec
+    real(8),parameter :: A00  =0.031091d0,A01  =0.015545d0,A02  =0.016887d0
+    real(8),parameter :: alp10=0.21370d0 ,alp11=0.20548d0 ,alp12=0.11125d0
+    real(8),parameter :: bt10 =7.5957d0  ,bt11 =1.41189d1 ,bt12 =1.0357d1
+    real(8),parameter :: bt20 =3.5876d0  ,bt21 =6.1977d0  ,bt22 =3.6231d0
+    real(8),parameter :: bt30 =1.6382d0  ,bt31 =3.3662d0  ,bt32 =0.88026d0
+    real(8),parameter :: bt40 =0.49294d0 ,bt41 =0.62517d0 ,bt42 =0.49671d0
+    real(8),parameter :: C1=2.14611945579107d0,C2=0.031091d0
+    real(8) :: const1, const2, factor
+    integer :: i,j,ispin,m,i1,i2,i3,j1,j2,j3,k1,k2,k3,ierr
+    real(8) :: trho, rhoa, rhob
+    real(8) :: kf, rs, ec_U, ec_P, ec_lda, phi, g2
+    real(8) :: dac_dn, dfz_dz, deU_dn, deP_dn, H, A, T, alpc, fz
+    real(8) :: dec_dz, dphi_dz, dH_dA, dH_dT, tmp, dH_dphi, Ai
+    real(8) :: dA_dn, dec_dn, Hs, zeta, dT_dphi
+    real(8) :: dz_dn(2), cm, rssq
+    real(8) :: dac_drs, deP_drs, deU_drs, drs_dn
+    real(8),allocatable :: rrrr(:,:), rtmp(:)
+    real(8) :: Pi, one, two, fouthr, onethr
+    real(8) :: sevthr, twothr, thrtwo, ThrFouPi
+
+    const1 = 2.0d0**(4.0d0/3.0d0)-2.0d0
+    const2 = 9.0d0*(2.0d0**(1.0d0/3.0d0)-1.0d0)/4.0d0
+
+    factor = 1.0d0
+    if ( MSP == 1 ) factor = 0.5d0
+ 
+    Pi       = acos(-1.0d0)
+    one      = 1.0d0
+    two      = 2.0d0
+    fouthr   = 4.0d0/3.0d0
+    onethr   = 1.0d0/3.0d0
+    ThrFouPi = 3.0d0/(4.0d0*Pi)
+    thrtwo   = 3.0d0/2.0d0
+    twothr   = 2.0d0/3.0d0
+    sevthr   = 7.0d0/3.0d0
+
+    vco = 0.0d0
+    Ec  = 0.0d0
+
+    allocate( rtmp(ML_0:ML_1) ) ; rtmp=0.0d0
+    allocate( rrrr(ML,3)      ) ; rrrr=0.0d0
+
+    do i=ML_0,ML_1
+
+       rhoa = rho(i,1  )*factor
+       rhob = rho(i,MSP)*factor
+       trho = rhoa + rhob
+
+       if ( trho <= zero_density ) cycle
+
+       zeta = ( rhoa - rhob )/trho
+
+       fz = ( (one+zeta)**fouthr + (one-zeta)**fouthr - two )*const1
+
+       kf = ( 3.0d0*Pi*Pi*trho )**onethr
+
+       rs = ( ThrFouPi/trho )**onethr
+
+       rssq = sqrt(rs)
+
+       ec_U = -two*A00*( one + alp10*rs ) &
+            *log( one + one/( two*A00 &
+            *(bt10*rssq + bt20*rs + bt30*rs*rssq + bt40*rs*rs) ) )
+       ec_P = -two*A01*( one + alp11*rs ) &
+            *log( one + one/( two*A01 &
+            *(bt11*rssq + bt21*rs + bt31*rs*rssq + bt41*rs*rs) ) )
+       alpc = -two*A02*( one + alp12*rs ) &
+            *log( one + one/( two*A02 &
+            *(bt12*rssq + bt22*rs + bt32*rs*rssq + bt42*rs*rs) ) )
+
+       ec_lda = ec_U - alpc*fz*const2*(one-zeta**4) &
+            + ( ec_P - ec_U )*fz*zeta**4
+
+       phi = 0.5d0*( (one+zeta)**twothr + (one-zeta)**twothr )
+
+       T = Pi*( gx(i)*gx(i) + gy(i)*gy(i) + gz(i)*gz(i) ) &
+             /( 16.0d0*phi**2*kf*trho**2 )
+
+       A = C1/( exp( -ec_lda/(C2*phi**3) ) - one )
+
+       Hs = C2 * phi**3 * log( one + C1*T*(one+A*T)/(one+A*T+(A*T)**2) )
+
+       Ec = Ec + trho*( ec_lda + Hs )
+
+       drs_dn = -4.0d0*Pi*rs**4/9.0d0
+
+       tmp = bt10*rssq + bt20*rs + bt30*rs*rssq + bt40*rs*rs
+       deU_drs = alp10*ec_U/( one + alp10*rs ) &
+            +A00*( one + alp10*rs )/rssq &
+            *( bt10 + two*bt20*rssq + 3.0d0*bt30*rs + 4.0d0*bt40*rs*rssq ) &
+            /( two*A00*tmp*tmp+tmp )
+
+       tmp = bt11*rssq + bt21*rs + bt31*rs*rssq + bt41*rs*rs
+       deP_drs = alp11*ec_P/( one + alp11*rs ) &
+            +A01*( one + alp11*rs )/rssq &
+            *( bt11 + two*bt21*rssq + 3.0d0*bt31*rs + 4.0d0*bt41*rs*rssq ) &
+            /( two*A01*tmp*tmp+tmp )
+
+       tmp = bt12*rssq + bt22*rs + bt32*rs*rssq + bt42*rs*rs
+       dac_drs = alp12*alpc/( one + alp12*rs ) &
+            +A02*( one + alp12*rs )/rssq &
+            *( bt12 + two*bt22*rssq + 3.0d0*bt32*rs + 4.0d0*bt42*rs*rssq ) &
+            /( two*A02*tmp*tmp+tmp )
+
+       deU_dn = deU_drs * drs_dn
+       deP_dn = deP_drs * drs_dn
+       dac_dn = dac_drs * drs_dn
+
+
+       dfz_dz = fouthr*( (one+zeta)**onethr - (one-zeta)**onethr )*const1
+
+       dec_dz = -alpc*dfz_dz*const2*(one-zeta**4) &
+               + 4.0d0*alpc*fz*const2*zeta**3 &
+               +(ec_P-ec_U)*( dfz_dz*zeta**4 + fz*4.0d0*zeta**3 )
+
+!       if ( zeta == 1.0d0 .or. zeta == -1.0d0 ) then
+!          dphi_dz = 0.0d0
+!       else
+          dphi_dz = ( (one+zeta)**(-onethr)-(one-zeta)**(-onethr) )/3.d0
+!       end if
+
+       tmp = one + A*T + (A*T)**2
+
+       dH_dA = -phi**3*C1*C2*A*T**3*(two+A*T)/(tmp**2+C1*T*(one+A*T)*tmp)
+
+       dH_dT =  phi**3*C1*C2*(one+two*A*T)/(tmp**2+C1*T*(one+A*T)*tmp)
+
+       dH_dphi = 3.0d0*Hs/phi
+
+       dz_dn(1)   = 2.0d0*rhob/trho**2
+       dz_dn(MSP) =-2.0d0*rhoa/trho**2
+
+       do ispin=MSP_0,MSP_1
+
+          dec_dn = deU_dn - dac_dn*fz*const2*(one-zeta**4) &
+               +(deP_dn-deU_dn)*fz*zeta**4 + dec_dz*dz_dn(ispin)
+
+          dA_dn = A*(C1+A)/(C1*C2*phi**3) &
+               *( dec_dn - 3.0d0*ec_lda/phi*dphi_dz*dz_dn(ispin) )
+
+          vco(i,ispin) = vco(i,ispin) + ec_lda + Hs + trho*dec_dn &
+               + trho*dH_dA*dA_dn &
+               + dH_dT*(-sevthr*T - trho*two*T/phi*dphi_dz*dz_dn(ispin) ) &
+               + trho*dH_dphi*dphi_dz*dz_dn(ispin)
+
+       end do ! ispin
+
+       rtmp(i) = dH_dT*Pi/(8.0d0*kf*trho*phi**2)
+
+    end do ! i
+
+    rrrr(ML_0:ML_1,1) = rtmp(ML_0:ML_1)*gx(ML_0:ML_1)
+    call mpi_allgatherv(rrrr(ML_0,1),ir_grid(myrank_g),mpi_real8 &
+         ,rrrr(1,1),ir_grid,id_grid,mpi_real8,comm_grid,ierr)
+
+    rrrr(ML_0:ML_1,2) = rtmp(ML_0:ML_1)*gy(ML_0:ML_1)
+    call mpi_allgatherv(rrrr(ML_0,2),ir_grid(myrank_g),mpi_real8 &
+         ,rrrr(1,2),ir_grid,id_grid,mpi_real8,comm_grid,ierr)
+
+    rrrr(ML_0:ML_1,3) = rtmp(ML_0:ML_1)*gz(ML_0:ML_1)
+    call mpi_allgatherv(rrrr(ML_0,3),ir_grid(myrank_g),mpi_real8 &
+         ,rrrr(1,3),ir_grid,id_grid,mpi_real8,comm_grid,ierr)
+
+    do i3=0,ML3-1
+    do i2=0,ML2-1
+    do i1=0,ML1-1
+       i=LLL2(i1,i2,i3)
+       do m=-Md,Md
+          cm=nab(m)*sign(1,m)
+          j1=i1+m
+          k1=j1/ML1 ; if ( j1<0 ) k1=(j1+1)/ML1-1
+          j1=j1-k1*ML1
+          j =LLL2(j1,i2,i3)
+          if ( ML_0 <= j .and. j <= ML_1 ) then
+             do ispin=MSP_0,MSP_1
+                vco(j,ispin) = vco(j,ispin) + cm*( rrrr(i,1)*b(1,1) &
+                                                  +rrrr(i,2)*b(2,1) &
+                                                  +rrrr(i,3)*b(3,1) )
+             end do
+          end if
+          j2=i2+m
+          k2=j2/ML2 ; if ( j2<0 ) k2=(j2+1)/ML2-1
+          j2=j2-k2*ML2
+          j =LLL2(i1,j2,i3)
+          if ( ML_0 <= j .and. j <= ML_1 ) then
+             do ispin=MSP_0,MSP_1
+                vco(j,ispin) = vco(j,ispin) + cm*( rrrr(i,1)*b(1,2) &
+                                                  +rrrr(i,2)*b(2,2) &
+                                                  +rrrr(i,3)*b(3,2) )
+             end do
+          end if
+          j3=i3+m
+          k3=j3/ML3 ; if ( j3<0 ) k3=(j3+1)/ML3-1
+          j3=j3-k3*ML3
+          j =LLL2(i1,i2,j3)
+          if ( ML_0 <= j .and. j <= ML_1 ) then
+             do ispin=MSP_0,MSP_1
+                vco(j,ispin) = vco(j,ispin) + cm*( rrrr(i,1)*b(1,3) &
+                                                  +rrrr(i,2)*b(2,3) &
+                                                  +rrrr(i,3)*b(3,3) )
+             end do
+          end if
+       end do ! m
+    end do ! i1
+    end do ! i2
+    end do ! i3
+
+    deallocate( rtmp )
+    deallocate( rrrr )
+
+    return
+  END SUBROUTINE calc_GGAPBE96_c2
 
 
 END MODULE xc_ggapbe96_module
