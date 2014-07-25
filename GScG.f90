e957eb5ea061a5083c40c23a995a374bbb48b976
lhan <lhan@comas.t.u-tokyo.ac.jp>
2014-07-25 17:37:22 +0900

    A GramSchmidtG.f90 InnerProduct.f90 RealComplex.f90 CG merged (refs #24 #26 #31 #32)

---
+++ GScG.f90
@@ -0,0 +1,166 @@
+MODULE GramSchmidtG
+  use rgrid_module, only: dV,zdV
+  use wf_module, only: unk
+  use array_bound_module, only: ML_0,ML_1,MB,MB_0
+  use parallel_module, only: COMM_GRID,COMM_BAND,ir_band,id_band,id_class
+  use GlobalVar_uspp, only: Sunk
+  implicit none
+
+  PRIVATE
+  PUBLIC :: GramSchmidtG
+
+CONTAINS
+
+!---------------------------------------------------------------------------------------
+  ! Gram-Schmidt orthogonalization
+  ! ( Takahashi, Block cyclic )
+  SUBROUTINE GramSchmidtG(n0,n1,k,s,NBLK,NBLK1)
+    implicit none
+    integer,intent(IN) :: n0,n1,k,s
+    integer,intent(IN) :: NBLK,NBLK1
+    
+    integer :: irank_b,ns,ne,ms,me,n,ML0,ierr
+    integer :: nbss,k1,ib,NBAND_BLK,ncycle,mrnk
+    integer,allocatable :: ir(:),id(:)
+
+    ! not used???? or in recursive subroutine
+    integer :: nn,cc1,cc2
+    integer :: nn1,nn2
+    integer :: mm1,mm2
+
+    ML0 = ML_1 - ML_0 + 1
+    mrnk = id_class(myrank,4)
+
+    if ( NBLK  == 0 ) NBLK =MB
+    if ( NBLK1 == 0 ) NBLK1=4
+
+    allocate( ir(0:np_band-1),id(0:np_band-1) )
+
+    ir(0:np_band-1)=ir_band(0:np_band-1)*ML0
+    id(0:np_band-1)=id_band(0:np_band-1)*ML0
+
+    NBAND_BLK=NBLK
+    ncycle=(MB-1)/NBAND_BLK+1
+
+    call MPI_ALLGATHERV( unk(ML_0,MB_0,k,s),ir(mrnk),TYPE_MAIN,unk(ML_0,1,k,s),ir,id,TYPE_MAIN,COMM_BAND,ierr )
+    do k1=1,ncycle
+       irank_b = mod(k1-1,np_band)
+       ns = NBAND_BLK*(k1-1) + 1
+       ne = min(ns+NBAND_BLK-1,MB)
+
+       allocate( Sunk(n1:n2,ns:ne) )
+       call get_Sunk_Mat( n1,n2,ns,ne,k,s )
+       if ( id_class(myrank,4)==irank_b ) then
+          call GramSchmidtGSub(ns,ne,ns,ne,NBLK,k,s,NBLK,NBLK1)
+       end if
+       n=ML0*(ne-ns+1)
+       call MPI_BCAST( unk(ML_0,ns,k,s),n,TYPE_MAIN,irank_b,COMM_BAND,ierr )
+       if ( ns <= MB-NBAND_BLK ) then
+          do ib=1,(ncycle-1)/np_band+1
+             nbss = (ib-1)*np_band + myrank_b + 1
+             if ( nbss<=ncycle .and. nbss>= k1+1 ) then
+                ms=NBAND_BLK*(nbss-1)+1
+                me=min(ms+NBAND_BLK-1,MB)
+                if ( ms<=me ) then
+                   call GramSchmidtGSub(ms,me,ns,ne,NBLK,k,s,NBLK,NBLK1)
+                end if
+             end if
+          end do
+       end if
+       deallocate(Sunk)
+    end do ! k1
+    deallocate( id,ir )
+
+    return
+  END SUBROUTINE GramSchmidtG
+
+!---------------------------------------------------------------------------------------
+  ! Gram-Schmidt orthogonalization
+  RECURSIVE SUBROUTINE GramSchmidtGSub(mm1,mm2,nn1,nn2,MBLK,k,s,NBLK,NBLK1)
+    implicit none
+    integer,intent(IN) :: mm1,mm2,nn1,nn2,MBLK,k,s
+    integer,intent(IN) :: NBLK,NBLK1
+    integer :: n,ns,ne,m,ms,me,m1,m2,mm,nn,MBLKH,ierr
+    integer :: ML0,i
+    real(8) :: c,d
+#ifdef _DRSDFT_
+    real(8),allocatable :: utmp2(:,:),vtmp2(:,:),utmp(:),vtmp(:)
+#else
+    complex(8),allocatable :: utmp2(:,:),vtmp2(:,:),utmp(:),vtmp(:)
+#endif
+
+    ML0 = ML1-ML_0+1
+
+    do ms=mm1,mm2,MBLK
+       me=min(ms+MBLK-1,mm2)
+       mm=me-ms+1
+       do ns=nn1,nn2,MBLK
+          ne=min(ns+MBLK-1,nn2)
+          ne=min(ne,me-1)
+          nn=ne-ns+1
+          if ( nn<=0 ) cycle
+          if ( ms>=ne+1 ) then
+             allocate( utmp2(ns:ne,ms:me),vtmp2(ns:ne,ms:me) )
+#ifdef _DRSDFT_
+             call dgemm(TRANSA,TRANSB,nn,mm,ML0, -dV,unk(ML_0,ns,k,s),ML0,Sunk(ML_0,ms),ML0,zero,utmp2,nn)
+#else
+             call zgemm(TRANSA,TRANSB,nn,mm,ML0,-zdV,unk(ML_0,ns,k,s),ML0,Sunk(ML_0,ms),ML0,zero,utmp2,nn)
+#endif
+
+             call MPI_ALLREDUCE( utmp2,vtmp2,nn*mm,TYPE_MAIN,MPI_SUM,COMM_GRID,ierr )
+
+#ifdef _DRSDFT_
+             call dgemm(TRANSB,TRANSB,ML0,mm,nn,one,unk(ML_0,ns,k,s),ML0,vtmp2,nn,one,unk(ML_0,ms,k,s),ML0)
+#else
+             call zgemm(TRANSB,TRANSB,ML0,mm,nn,one,unk(ML_0,ns,k,s),ML0,vtmp2,nn,one,unk(ML_0,ms,k,s),ML0)
+#endif
+
+             deallocate( vtmp2,utmp2 )
+             if ( ms==ne+1 ) then
+                call get_gSf(unk(ML_0,ms,k,s),unk(ML_0,ms,k,s),ML_0,ML_1,k,d,0)
+                call MPI_ALLREDUCE( d,c,1,mpi_real8,MPI_SUM,COMM_GRID,ierr )
+                ! ?????????????????????????????? dV????
+                !						c=1.d0/sqrt(c)
+                c=1.d0/sqrt(c*dV)
+                unk(ML_0:ML_1,ms,k,s) = c*unk(ML_0:ML_1,ms,k,s)
+             end if
+
+          else if ( mm<=NBLK1 ) then
+             allocate( utmp(NBLK1),vtmp(NBLK1) )
+             do m=ms,me
+                n = min(m-1,ne)
+                if ( n-ns+1>0 ) then
+#ifdef _DRSDFT_
+                   call dgemv(TRANSA,ML0,n-ns+1, -dV,unk(ML_0,ns,k,s),ML0,Sunk(ML_0,m),1,zero,utmp,1)
+#else
+                   call zgemv(TRANSA,ML0,n-ns+1,-zdV,unk(ML_0,ns,k,s),ML0,Sunk(ML_0,m),1,zero,utmp,1)
+#endif
+                   call mpi_allreduce(utmp,vtmp,n-ns+1,TYPE_MAIN,mpi_sum,comm_grid,ierr)
+
+#ifdef _DRSDFT_
+                   call dgemv(TRANSB,ML0,n-ns+1,one,unk(ML_0,ns,k,s),ML0,vtmp,1,one,unk(ML_0,m,k,s),1)
+#else
+                   call zgemv(TRANSB,ML0,n-ns+1,one,unk(ML_0,ns,k,s),ML0,vtmp,1,one,unk(ML_0,m,k,s),1)
+#endif
+                end if
+                if ( m==1 .or. (n==m-1 .and. m/=ns) ) then
+                   call get_gSf(unk(ML_0,m,k,s),unk(ML_0,m,k,s),ML_0,ML_1,k,d,0)
+                   call mpi_allreduce(d,c,1,mpi_real8,mpi_sum,comm_grid,ierr)
+                   !							c=1.d0/sqrt(c)
+                   c=1.d0/sqrt(c*dV)
+                   unk(ML_0:ML_1,m,k,s)=c*unk(ML_0:ML_1,m,k,s)
+                end if
+             end do ! m
+             deallocate( vtmp,utmp )
+
+          else
+             MBLKH=max(MBLK/2,NBLK1)
+             call GramSchmidtGSub(ms,me,ns,ne,MBLKH,k,s,NBLK,NBLK1)
+          end if
+       end do ! ns
+    end do ! ms
+
+    return
+  END SUBROUTINE GramSchmidtGSub
+
+END MODULE GramSchmidtG
---
+++ InnerProduct.f90
@@ -0,0 +1,325 @@
+MODULE InnerProduct
+  use watch_module, only: watch
+  ! Mlma,nzlma,MMJ(lma),JJP(MJJ(lma),lma),TYPE_MAIN,lma_nsend(irank),sendmap(i1,irank),
+  ! sbufnl(i2,irank),rbufnl(1,jrank),uVk(MJJ(lma),lma,k),
+  ! nrlma_xyz(1:6),num_2_rank(nrlma_xyz(:),1:6)
+  use ps_nloc2_variables
+  use pseudopot_module, only: pselect
+  use rgrid_module, only: dV
+  use parallel_module, only: COMM_GRID
+  ! N_nlop,nlop_pair(1:2,:),qij(m)
+  use ps_nloc_u_module, only: N_nlop,nlop_pair,qij
+  ! unk(nn1,n,k,s)
+  use wf_module, only: unk
+  ! Sunk(nn1,n)
+  use GlobalVar_uspp, only: Sunk
+  use RealComplex, only: zero,RCProduct
+  
+  use ParaRGridComm, only: threeWayComm
+
+  PRIVATE
+  PUBLIC :: dot_product,get_Sf,get_gf,get_gSf,get_Sunk_Mat
+
+CONTAINS
+
+!---------------------------------------------------------------------------------------	
+
+  SUBROUTINE dot_product( a,b,c,alpha,n,m,nop )
+!$ use omp_lib
+    implicit none
+    integer,intent(IN) :: n,m
+    integer :: i
+    real(8) :: a(*),b(*),c(*),alpha,nop,tmp
+
+    c(1:m)=0.d0
+
+    tmp=0.d0
+!$OMP parallel do reduction(+:tmp)
+    do i=1,n
+       tmp = tmp + a(i)*b(i)
+    end do
+!$OMP end parallel do
+    c(1) = tmp*alpha
+    nop = nop + 2.d0*n +1.d0
+
+    if ( m==2 ) then
+       tmp=0.d0
+!$OMP parallel do reduction(+:tmp)
+       do i=1,n-1,2
+          tmp = tmp + a(i)*b(i+1) - a(i+1)*b(i)
+       end do
+!$OMP end parallel do
+       c(m) = tmp*alpha
+       nop = nop + 4.d0*(n/2) + 1.d0
+    end if
+    return
+  END SUBROUTINE dot_product
+
+!---------------------------------------------------------------------------------------
+  SUBROUTINE get_Sf(fin,nn1,nn2,k,Sf)
+    ! IN:	nn1,nn2,k,fin
+    !		Mlma,nzlma,pselect,
+    !		MMJ(lma),JJP(MJJ(lma),lma)
+    !		uVk(MJJ(lma),lma,k),
+    !		dV,
+    !		nrlma_xyz(1:6),num_2_rank(nrlma_xyz(:),1:6),
+    !		lma_nsend(irank),sbufnl(i2,irank),rbufnl(1,jrank),sendmap(i1,irank),
+    !		TYPE_MAIN,COMM_GRID
+    !		N_nlop,qij(m),nlop_pair(1,N_nlop)
+    ! OUT:	Sf(JJP(MMJ(nlop_pair(1,N_nlop)),nlop_pair(1,N_nlop))
+!$ use omp_lib
+    implicit none
+
+    integer,intent(IN) :: nn1,nn2,k
+#ifdef _DRSDFT_
+    real(8),intent(IN) :: fin(nn1:nn2)
+    real(8),intent(OUT) :: Sf(nn1:nn2)
+    real(8) :: p_uVunk2
+    real(8) :: uVunk2
+    real(8) :: uVunk_z(Mlma)
+    real(8),parameter :: zero=0.d0
+    real(8) :: tmp
+#else
+    complex(8),intent(IN) :: fin(nn1:nn2)
+    complex(8),intent(OUT) :: Sf(nn1:nn2)
+    complex(8) :: p_uVunk2
+    complex(8) :: uVnk2
+    complex(8) :: uVnk_z(Mlma)
+    complex(8),parameter :: zero=(0.d0,0.d0)
+    complex(8) :: tmp
+#endif
+    integer :: kk1,i,j,ierr,ii
+    integer :: ib1,ib2,ib,nb
+    integer :: lma,lma1,lma2,m
+    integer :: i1,i2,irank,jrank
+    integer :: nreq
+    integer :: istatus(MPI_STATUS_SIZE,512),ireq(512)
+    real(8) :: ctt(0:1),ett(0:1)
+
+! ??????????????????????????????????
+    ib1=1
+    ib2=1
+    nb= ib2 - ib1 + 1
+
+    select case ( pselect )
+    case ( 1,2,12 )
+!$OMP parallel do
+       do i=nn1,nn2
+          Sf(i) = fin(i)
+       end do
+!$OMP end parallel do
+
+!----- get uVunk (=<uV|fin>) -----
+       allocate( uVunk(nzlma,ib1:ib2),uVunk0(nzlma,ib1:ib2) )
+!----- within mygrid -----
+#ifndef _OMP_
+       do ib=ib1,ib2
+          do lma=1,nzlma
+             uVunk(lma,ib)=zero
+             do j=1,MJJ(lma)
+                i=JJP(j,lma)
+                call RCProduct( uVk(j,lma,k),fin(i),tmp )
+                uVunk(lma,ib) = uVunk(lma,ib) + tmp
+             end do
+             uVunk(lma,ib) = dV*uVunk(lma,ib)
+          end do
+       end do
+#else
+!$OMP parallel
+!$OMP workshare
+       uVunk(:,:)=zero
+!$OMP end workshare
+       do ib=ib1,ib2
+!$OMP do
+          do lma=1,nzlma
+             do j=1,MJJ(lma)
+                i=JJP(j,lma)
+                call RCProduct( uVk(j,lma,k),fin(i),tmp )
+                uVunk(lma,ib) = uVunk(lma,ib) + tmp
+             end do
+             uVunk(lma,ib) = dV*uVunk(lma,ib)
+          end do
+!$OMP end do
+       end do
+!$OMP end parallel
+#endif
+!===== within mygrid =====
+
+!----- summation over all grids -----
+       call watch( ctt(0),ett(0) )
+
+! 3WayComm
+! uVunk
+       call ThreeWayComm( nrlma_xyz,num_2_rank,sendmap,recvmap,lma_nsend,sbufnl,rbufnl,nzlma,ib1,ib2,uVunk )
+
+       call watch( ctt(1),ett(1) )
+!===== summation over all grids =====
+
+!----- total = term1 + term2 -----
+#ifndef _OMP_
+       do ib=ib1,ib2
+          do m=1,N_nlop
+             lma1 = nlop_pair(1,m)
+             lma2 = nlop_pair(2,m)
+             do j=1,MJJ(lma1)
+                i=JJP(j,lma1)
+                Sf(i) = Sf(i) + qij(m)*uVk(j,lma1,k)*uVunk(lma2,ib)
+             end do
+          end do
+       end do
+#else
+       do ib=ib1,ib2
+!$OMP parallel private(lma1,lma2,i)
+          do m=1,N_nlop
+             lma1 = nlop_pair(1,m)
+             lma2 = nlop_pair(2,m)
+!$OMP do
+             do j=1,MJJ(lma1)
+                i=JJP(j,lma1)
+                Sf(i) = Sf(i) + qij(m)*uVk(j,lma1,k)*uVunk(lma2,ib)
+             end do
+!$OMP end do
+          end do
+!$OMP end parallel
+       end do
+#endif
+!===== total = term1 + term2 =====
+
+       deallocate( uVunk0,uVunk )
+
+    case ( 3 )
+!----- term1 -----
+!$OMP parallel do
+       do i=nn1,nn2
+          Sf(i) = fin(i)
+       end do
+!$OMP end parallel do
+!===== term1 =====
+
+!----- term2 -----
+       do i=1,Mlma
+          uVunk2=zero
+          p_uVunk2=zero
+!$OMP parallel do reduction(+:p_uVunk2)
+          do j=nn1,nn2
+             call RCProduct( uVk(i,j,k),fin(j),tmp )
+             p_uVunk2 = p_uVunk2 + tmp
+          end do
+!$OMP end parallel do
+          p_uVunk2 = dV*p_uVunk2
+
+          call MPI_ALLREDUCE(p_uVunk2,uVunk2,1,TYPE_MAIN,MPI_SUM,COMM_GRID,ierr)
+
+          uVunk_z(i) = uVunk2
+       end do
+
+       do kk1=1,N_nlop
+          i = nlop_pair(1,m)
+          j = nlop_pair(2,m)
+!$OMP parallel do
+          do ii=nn1,nn2
+             Sf(ii) = Sf(ii) + uVk(ii,i,k)*qij(kk1)*uVunk_z(j)
+          end do
+!$OMP end parallel do
+       end do
+!===== term2 =====
+    end select
+
+    return
+  END SUBROUTINE get_Sf
+
+!---------------------------------------------------------------------------------------
+
+  SUBROUTINE get_gf( gin,fin,nn1,nn2,gf,switch_zp )
+    ! IN:	nn1,nn2,gin(nn1:nn2),fin(nn1:nn2),switch_zp,
+    !		dV,
+    !		TYPE_MAIN,COMM_GRID
+    ! OUT:	gf
+!$ use omp_lib
+    implicit none
+
+    integer,intent(IN) :: nn1,nn2
+    integer,intent(IN) :: switch_zp
+#ifdef _DRSDFT_
+    real(8),intent(IN) :: fin(nn1:nn2),gin(nn1:nn2)
+    real(8),intent(OUT) :: gf
+    real(8) :: gf_0
+    real(8) :: tmp
+#else
+    complex(8),intent(IN) :: fin(nn1:nn2),gin(nn1:nn2)
+    complex(8),intent(OUT) :: gf
+    complex(8) :: gf_0
+    complex(8) :: tmp
+#endif
+    integer :: i
+    integer :: ierr
+
+    gf_0=zero
+
+!$OMP parallel do reduction(+:gf_0)
+    do i=nn1,nn2
+       call RCProduct( gin(i),fin(i),tmp )
+       gf_0 = gf_0 + tmp
+    end do
+!$OMP end parallel do
+    gf_0 = dV*gf_0
+
+    if ( switch_zp==1 ) then
+       call MPI_ALLREDUCE(gt_0,gf,1,TYPE_MAIN,MPI_SUM,COMM_GRID,ierr)
+    else
+       gf = gf_0
+    end if
+
+    return
+  END SUBROUTINE get_gf
+
+  !---------------------------------------------------------------------------------------
+
+  SUBROUTINE get_gSf( gin,fin,nn1,nn2,k,gSf,switch_zp )
+    ! IN:	nn1,nn2,gin(nn1:nn2),fin(nn1:nn2),k,switch_zp,
+    ! OUT:	gSf
+!$ use omp_lib
+    implicit none
+
+    integer,intent(IN) :: nn1,nn2,k		
+    integer,intent(IN) :: switch_zp
+#ifdef _DRSDFT_
+    real(8),intent(IN) :: gin(nn1:nn2),fin(nn1:nn2)
+    real(8),intent(OUT) :: gSf
+    real(8) :: Sf(nn1:nn2),gSf_0
+#else
+    complex(8),intent(IN) :: gin(nn1:nn2),fin(nn1:nn2)
+    complex(8),intent(OUT) :: gSf
+    complex(8) :: Sf(nn1:nn2),gSf_0
+#endif
+    integer :: i
+    integer :: ierr
+
+!----- |f'> = S|f> = Sf -----
+    call get_Sf( fin,nn1,nn2,k,Sf )
+
+!----- <g|f'> = <g|S|f> = gSf
+    call get_gf( gin,Sf,nn1,nn2,gSf,switch_zp )
+
+    return
+  END SUBROUTINE get_gSf
+
+!---------------------------------------------------------------------------------------
+
+  SUBROUTINE get_Sunk_Mat( nn1,nn2,ns,ne,k,s )
+    ! IN:	nn1,nn2,ns,ne,k,s,
+    !		unk(nn1,n,k,s)
+    ! OUT:	Sunk(nn1,n)
+    implicit none
+
+    integer,intent(IN) :: nn1,nn2,ns,ne,k,s
+    integer :: n
+
+    do n=ns,ne
+       call get_Sf( unk(nn1,n,k,s),nn1,nn2,k,Sunk(nn1,n) )
+    end do
+
+    return
+  END SUBROUTINE get_Sunk_Mat
+
+END MODULE InnerProduct
