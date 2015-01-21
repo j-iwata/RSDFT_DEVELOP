927886e7130f8a3a2e546126a801ef6177edad57
J. Iwata <iwata@kaki.localhost>
2015-01-21 19:53:38 +0900

    bug (iflag_hunk) 	modified:   gram_schmidt_t_module.f90

--- gram_schmidt_t_module.f90
+++ gram_schmidt_t_module.f90
@@ -120,7 +120,7 @@ CONTAINS
 
        n=ML0*(ne-ns+1)
        call mpi_bcast(unk(ML_0,ns,k,s),n,TYPE_MAIN,irank_b,comm_band,ierr)
-       if ( iflag_hunk >= 0 ) then
+       if ( iflag_hunk >= 1 ) then
           call mpi_bcast(hunk(ML_0,ns,k,s),n,TYPE_MAIN,irank_b,comm_band,ierr)
        end if
 
