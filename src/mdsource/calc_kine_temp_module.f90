a87fd58701e8baedeaab538cd795a0692d6717be
iwata <iwata@kaki.localhost>
2016-02-20 15:25:03 +0900

    bug ( CPMD with force constraint ) 	modified:   Makefile 	modified:   force_module.f90 	modified:   mdsource/Makefile 	new file:   mdsource/berendsen_module.f90 	modified:   mdsource/bomd.f90 	new file:   mdsource/calc_kine_temp_module.f90 	modified:   mdsource/cpmd_variables.f90 	modified:   mdsource/setv.f90 	modified:   mdsource/vcom.f90

---
+++ src/mdsource/calc_kine_temp_module.f90
@@ -0,0 +1,55 @@
+MODULE calc_kine_temp_module
+
+  use atom_module, only: Natom, ki_atom, zn_atom, md_atom
+  use cpmd_variables, only: pmass, AMU, Ndof
+  use force_module, only: tim
+
+  implicit none
+
+  PRIVATE
+  PUBLIC :: calc_Ndof
+  PUBLIC :: calc_kine
+  PUBLIC :: calc_temp
+
+CONTAINS
+
+  SUBROUTINE calc_Ndof   ! Degree of freedom
+    implicit none
+    real(8) :: v(3),u(3)
+    integer :: i,m
+    call random_number( v )
+    Ndof=0
+    do i=1,Natom
+       m=md_atom(i)
+       u(:) = matmul( tim(:,:,m), v(:) )
+       Ndof = Ndof + count( u /= 0.0d0 )
+    end do
+  END SUBROUTINE calc_Ndof
+
+  SUBROUTINE calc_kine( Velocity, kine )
+    implicit none
+    real(8),intent(IN)  :: Velocity(:,:)
+    real(8),intent(OUT) :: kine
+    integer :: i
+    real(8) :: pm
+    kine=0.0d0
+    do i=1,Natom
+       pm = pmass( zn_atom(ki_atom(i)) )
+       kine = kine + sum( Velocity(:,i)**2 )*pm
+    end do
+    kine=kine*0.5d0*AMU
+  END SUBROUTINE calc_kine
+
+  SUBROUTINE calc_temp( Velocity, temp )
+    implicit none
+    real(8),intent(IN)  :: Velocity(:,:)
+    real(8),intent(OUT) :: temp
+    real(8),parameter :: TOJOUL=4.35975d-18 ! (J/hartree)
+    real(8),parameter :: kB_J=1.380658d-23  ! (J/K)
+    real(8),parameter :: kB=kB_J/TOJOUL     ! (hartree/K)
+    real(8) :: kine
+    call calc_kine( Velocity, kine )
+    temp = kine/( 0.5d0*Ndof*kB )
+  END SUBROUTINE calc_temp
+
+END MODULE calc_kine_temp_module
--- src/mdsource/vcom.f90
+++ src/mdsource/vcom.f90
@@ -1,30 +1,42 @@
 !-----------------------------------------------------------------------
-!     Set center of motion off
+!     Make center of mass motion off
 !-----------------------------------------------------------------------
-subroutine vcom(kine0)
+
+SUBROUTINE vcom( Velocity )
+
   use atom_module, only: Natom,ki_atom,zn_atom
-  use cpmd_variables, only: pmass,AMU,Velocity
+  use cpmd_variables, only: pmass,AMU
+  use calc_kine_temp_module, only: calc_kine
+
   implicit none
-  real(8),intent(out)   :: kine0
-  real(8) rescale,pm,kine,vw(4)
-  integer i,k
-  vw(:)=0.d0
+  real(8),intent(INOUT) :: Velocity(3,Natom)
+  real(8) rescale,pm,kine0,kine,MG,VG(3)
+  integer i
+
+  call calc_kine( Velocity, kine0 )
+
+  MG=0.0d0
+  VG=0.0d0
+
   do i=1,Natom
-     pm=pmass(zn_atom(ki_atom(i)))*AMU
-     do k=1,3
-        vw(k)=vw(k)+Velocity(k,i)*pm
-     enddo
-     vw(4)=vw(4)+pm
-  enddo
-  call calkin(kine0)
-  vw(1:3)=vw(1:3)/vw(4)
-  do i=1,Natom
-     do k=1,3
-        Velocity(k,i)=Velocity(k,i)-vw(k)
-     enddo
+     pm = pmass( zn_atom(ki_atom(i)) )*AMU
+     MG = MG + pm
+     VG(1) = VG(1) + Velocity(1,i)*pm
+     VG(2) = VG(2) + Velocity(2,i)*pm
+     VG(3) = VG(3) + Velocity(3,i)*pm
   end do
-  call calkin(kine)
-  rescale=sqrt(kine0/kine)
-  Velocity(:,:)=Velocity(:,:)*rescale
-  return
-end subroutine vcom
+
+  VG(1:3)=VG(1:3)/MG
+  do i=1,Natom
+     Velocity(1,i) = Velocity(1,i) - VG(1)
+     Velocity(2,i) = Velocity(2,i) - VG(2)
+     Velocity(3,i) = Velocity(3,i) - VG(3)
+  end do
+
+  call calc_kine( Velocity, kine )
+
+  rescale = sqrt( kine0/kine )
+
+  Velocity(:,:) = Velocity(:,:)*rescale
+
+END SUBROUTINE vcom
