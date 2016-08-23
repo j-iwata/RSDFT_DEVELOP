6caa304af34c90573c1231d23dedbb1a8ab40e17
iwata <iwata@kaki.localhost>
2016-08-23 11:51:40 +0900

    minor bug & change ( different # of bands for each k-points ) 	modified:   utility/ufld2gp.f90

--- utility/ufld2gp.f90
+++ utility/ufld2gp.f90
@@ -3,7 +3,7 @@ PROGRAM ufld2gp
   implicit none
   integer,parameter :: u1=1, u2=10, u3=11
   integer,parameter :: maxloop=1000000
-  integer :: loop,ik,k,n,i,j,s,nk,nk0,nb,ns,ne,kmax
+  integer :: loop,ik,k,n,i,j,s,nk,nk0,nb,ns,ne,kmax,nmax,nmin
   integer :: unit_conv=1
   character(5) :: cbuf
   character(128) :: cbuf_long
@@ -25,7 +25,7 @@ PROGRAM ufld2gp
 
   write(*,*) "eta(eV), ne="
   read(*,*) eta, ne
-  eta = eta/HT
+  eta = eta/HT_
 
 ! ---
 
@@ -35,14 +35,18 @@ PROGRAM ufld2gp
   nb=0
   nk=0
   kmax=0
+  nmax=0
+  nmin=1000000000
   do loop=1,maxloop
      read(u1,*,END=1) cbuf,k
      if ( cbuf == "iktrj" ) then
         kmax=max(k,kmax)
         nk=nk+1
+        if ( nb /= 0 ) nmin=min(nmin,nb)
         nb=0
      else
         nb=nb+1
+        nmax=max(nmax,nb)
      end if
   end do
 1 continue
@@ -50,23 +54,24 @@ PROGRAM ufld2gp
   rewind u1
   read(u1,*)
   read(u1,'(a)') cbuf_long
-  read(cbuf_long,*,END=2) i,j,dummy
+  read(cbuf_long,*,END=2) i,j,dummy(1:4)
   ns=2
 2 continue
   if ( ns == 0 ) ns=1
 
   write(*,*) "ns, nk, nb=",ns,nk,nb
   write(*,*) "kmax=",kmax
+  write(*,*) "nmin,nmax=",nmin,nmax
 
   nk0=nk
   nk=kmax
 
 ! ---
 
-  allocate( kxyz_pc(3,0:nk)  ) ; kxyz_pc=0.0d0
-  allocate( kxyz_sc(3,nk)    ) ; kxyz_sc=0.0d0
-  allocate( esp(nb,nk,ns)    ) ; esp=0.0d0
-  allocate( weight(nb,nk,ns) ) ; weight=0.0d0
+  allocate( kxyz_pc(3,0:nk)    ) ; kxyz_pc=0.0d0
+  allocate( kxyz_sc(3,nk)      ) ; kxyz_sc=0.0d0
+  allocate( esp(nmax,nk,ns)    ) ; esp=0.0d0
+  allocate( weight(nmax,nk,ns) ) ; weight=0.0d0
 
 ! ---
 
@@ -76,18 +81,21 @@ PROGRAM ufld2gp
 
      read(u1,*) cbuf,k,kxyz_pc(1:3,k),kxyz_sc(1:3,k)
 
-     do n=1,nb
-
+     do n=1,nmax
         read(u1,*) i,j,(esp(n,k,s),weight(n,k,s),s=1,ns)
+        read(u1,*,END=9) cbuf
+        backspace(u1)
+        if ( cbuf == "iktrj" ) exit
 
      end do
 
   end do
+9 continue
 
 ! ---
 
-  emin = minval( esp )
-  emax = maxval( esp )
+  emin = minval( esp(1:nmin,:,:) )
+  emax = maxval( esp(1:nmin,:,:) )
 
   write(*,*) "emin, emax=",emin,emax
 
@@ -135,7 +143,7 @@ PROGRAM ufld2gp
 
         do s=1,ns
            f(s)=0.0d0
-           do n=1,nb
+           do n=1,nmin
               f(s) = f(s) + eta/( (e-esp(n,k,s))**2 + eta**2 )*weight(n,k,s)
            end do
            f(s)=f(s)/pi
