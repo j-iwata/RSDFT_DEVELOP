e48511edd570c42cf09414e619534efd9a5e8790
iwata <iwata@kaki.localhost>
2017-11-15 16:52:28 +0900

    bug: band-parallel in cpmd 	deleted:    mdsource/calkin.f90 	deleted:    mdsource/cpmdio.f90 	modified:   mdsource/getforce_cpmd.f90 	deleted:    mdsource/init_ion.f90 	deleted:    mdsource/mnhc.f90 	deleted:    mdsource/overlap.f90 	deleted:    mdsource/overlap.f90.dgemm 	deleted:    mdsource/rotorb.f90

--- src/mdsource/getforce_cpmd.f90
+++ src/mdsource/getforce_cpmd.f90
@@ -6,7 +6,7 @@ SUBROUTINE getforce_cpmd( ltime )
   use eion_module, only: calc_eion
   use atom_module, only: Natom,aa_atom,ki_atom,zn_atom
   use bb_module
-  use parallel_module, only: myrank
+  use parallel_module, only: myrank, np_band, myrank_b, comm_band
   use strfac_module
   use ps_local_module
   use ps_pcc_module
@@ -20,7 +20,7 @@ SUBROUTINE getforce_cpmd( ltime )
   use xc_module
   use hartree_variables, only: Vh
   use hartree_module, only: calc_hartree
-  use cpmd_variables, only: Force,Rion,disp_switch,AMU,pmass
+  use cpmd_variables, only: Force,Rion,disp_switch,AMU,pmass,ir_band_cpmd,id_band_cpmd
   use watch_module
   use force_module
 
@@ -28,13 +28,16 @@ SUBROUTINE getforce_cpmd( ltime )
   use ps_prepNzqr_g_module
   use ps_qrij_prep_module
 
+  use wf_module, only: unk
+  use rsdft_mpi_module
+
   implicit none
 
   logical,intent(IN) :: ltime
-  integer :: i,s,MB_0_BAK,MB_1_BAK
+  integer :: i,s
   real(8) :: ctime_force(0:9),etime_force(0:9),c
 
-  c=1.d0/(2.d0*acos(-1.d0))
+  c=1.0d0/(2.0d0*acos(-1.0d0))
   aa_atom = matmul(transpose(bb),Rion)*c
   Force   = 0.0d0
 
@@ -66,6 +69,7 @@ SUBROUTINE getforce_cpmd( ltime )
 
   if ( ltime ) call watch(ctime_force(3),etime_force(3))
 
+  if ( np_band > 1 ) call wf_gather_sub( unk )
   call calc_density
 
   if ( ltime ) call watch(ctime_force(4),etime_force(4))
@@ -101,5 +105,28 @@ SUBROUTINE getforce_cpmd( ltime )
   endif
 
   return
+
+CONTAINS
+
+  SUBROUTINE wf_gather_sub( u )
+    implicit none
+    real(8),intent(INOUT) :: u(:,:,:,:)
+    integer,save,allocatable :: ir(:),id(:)
+    integer,save :: ib1,ib2
+    integer :: k,s
+    if ( .not.allocated(ir) ) then
+       allocate( ir(0:np_band-1) ) ; ir=0
+       allocate( id(0:np_band-1) ) ; id=0
+       ir(0:np_band-1)=ir_band_cpmd(0:np_band-1)*size(u,1)
+       id(0:np_band-1)=id_band_cpmd(0:np_band-1)*size(u,1)
+       ib1=id_band_cpmd(myrank_b)+1
+       ib2=id_band_cpmd(myrank_b)+ir_band_cpmd(myrank_b)
+    end if
+    do s=1,size(u,4)
+    do k=1,size(u,3)
+       call rsdft_allgatherv( u(:,ib1:ib2,k,s),u(:,:,k,s),ir,id,comm_band )
+    end do
+    end do
+  END SUBROUTINE wf_gather_sub
 
 END SUBROUTINE getforce_cpmd
